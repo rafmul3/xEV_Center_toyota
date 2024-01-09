68ece7f3d9b2a6d937bda7a30536824fa1454115
rafmul3 <mrafimuli35@gmail.com>
2024-01-09 12:36:31 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Hash;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pengunjung.registrasi');
 });
 
 Route::resource('pengunjung', PengunjungController::class);
